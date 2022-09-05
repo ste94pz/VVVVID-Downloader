f593c091f49cfa71e0cc59f2252d2a71dc589b8b
ste94pz <49609029+ste94pz@users.noreply.github.com>
2022-09-05 13:37:38 +0200

    Update vvvvid-dowloader.sh
    Avvio senza docker

--- vvvvid-dowloader.sh
+++ vvvvid-dowloader.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Build image if not exists
-if [[ $(docker image inspect vvvvid-downloader:latest 2> /dev/null) == "[]" ]]; then
-    docker build -t vvvvid-downloader .
-fi
-
-# Execute vvvvid-downloader
-docker run -it --rm -v $(pwd)/downloads:/app/Downloads -v $(pwd)/downloads_list.txt:/app/downloads_list.txt vvvvid-downloader
+python3 src/main.py
